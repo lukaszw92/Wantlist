5cae14ee42e6f8e312d52897edd518213cbbf54f
lukaszw92 <lukasz.wieczorek@op.pl>
2020-10-09 21:39:10 +0200

    user model from get_user_model method

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,9 +2,6 @@ from rest_framework import serializers
 from .models import User
 from django.contrib.auth import get_user_model
 
-user = get_user_model()
-
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework.views import APIView
 
 from accounts.serializers import UserSerializer
 from rest_framework.response import Response
-from .models import User
 
 
 class UserListView(APIView):
 
     def get(self, request):
-        users = User.objects.all()
+        users = get_user_model().objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
