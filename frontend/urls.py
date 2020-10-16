d2c0a29245b3b50c1e68d1a74dccd06e46b38cbd
lukaszw92 <lukasz.wieczorek@op.pl>
2020-10-16 23:16:13 +0200

    artist creation form

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,5 @@ from frontend import views
 
 urlpatterns = [
     path('artist_list/', views.artistList, name='artist_list'),
+    path('artist_create/', views.artistCreate, name='artist_create')
     ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def artistList(request):
     return render(request, 'frontend/artist_list.html')
+
+def artistCreate(request):
+    return render(request, 'frontend/artist_create.html')
